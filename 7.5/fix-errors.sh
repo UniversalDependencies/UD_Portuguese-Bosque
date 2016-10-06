5f2f2cb4ac8ce9e8d7ed824ecffefea705a6216a
Fabricio Rosario <fchalub@br.ibm.com>
2016-10-06 14:24:29 -0300

    added workaround for #7

--- 7.5/fix-errors.sh
+++ 7.5/fix-errors.sh
@@ -2,3 +2,6 @@
 cat bosque_CP.udep.conll | sed -e 's/Gender=None/Gender=Com/' > tmp && mv tmp bosque_CP.udep.conll
 cat bosque_CF.udep.conll | sed -e 's/Gender=None/Gender=Com/' > tmp && mv tmp bosque_CF.udep.conll
 
+# fix issue #7
+cat bosque_CP.udep.conll | sed -e 's/PronType=Indp/PronType=Ind/' > tmp && mv tmp bosque_CP.udep.conll
+cat bosque_CF.udep.conll | sed -e 's/PronType=Indp/PronType=Ind/' > tmp && mv tmp bosque_CF.udep.conll
