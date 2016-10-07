09dae416146193b69c3bcd0dd91720006aab3392
Fabricio Rosario <fchalub@br.ibm.com>
2016-10-07 13:01:38 -0300

    removing workaround for issue #2 as there is no consensus

--- 7.5/fix-errors.sh
+++ 7.5/fix-errors.sh
@@ -1,6 +1,8 @@
 # fix issue #2
-cat bosque_CP.udep.conll | sed -e 's/Gender=None/Gender=Com/' > tmp && mv tmp bosque_CP.udep.conll
-cat bosque_CF.udep.conll | sed -e 's/Gender=None/Gender=Com/' > tmp && mv tmp bosque_CF.udep.conll
+# commenting out this for now as there is no consensus on how to
+# handle this
+# cat bosque_CP.udep.conll | sed -e 's/Gender=None/Gender=Com/' > tmp && mv tmp bosque_CP.udep.conll
+# cat bosque_CF.udep.conll | sed -e 's/Gender=None/Gender=Com/' > tmp && mv tmp bosque_CF.udep.conll
 
 # fix issue #7
 cat bosque_CP.udep.conll | sed -e 's/PronType=Indp/PronType=Ind/' > tmp && mv tmp bosque_CP.udep.conll
