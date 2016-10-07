406b8ebe305e39a177ed8371911e4e877b91f1c9
Fabricio Rosario <fchalub@br.ibm.com>
2016-10-07 14:40:26 -0300

    fixing paths

--- train/reformat.sh
+++ train/reformat.sh
@@ -4,6 +4,6 @@ LANG=pt
 
 if [ ! -d $LANG ]; then mkdir -p $LANG; fi
 
-cat ../7.5-fl/devel.conll | awk 'NF==0 {print} NF>0 {print $1 " " $2 " " $3 " " $4 " " $5 " " $6 " " $7 " " $8 " _ _ _ _ _ _" }' > $LANG/$LANG.devel
+cat ../bosque-ud-fl/devel.conll | awk 'NF==0 {print} NF>0 {print $1 " " $2 " " $3 " " $4 " " $5 " " $6 " " $7 " " $8 " _ _ _ _ _ _" }' > $LANG/$LANG.devel
 
-cat ../7.5-fl/train.conll | awk 'NF==0 {print} NF>0 {print $1 " " $2 " " $3 " " $4 " " $5 " " $6 " " $7 " " $8 " _ _ _ _ _ _" }' > $LANG/$LANG.train
+cat ../bosque-ud-fl/train.conll | awk 'NF==0 {print} NF>0 {print $1 " " $2 " " $3 " " $4 " " $5 " " $6 " " $7 " " $8 " _ _ _ _ _ _" }' > $LANG/$LANG.train
