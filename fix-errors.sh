926bc915b00e48e47c443cd44b4384400662e623
Alexandre Rademaker <arademaker@gmail.com>
2016-10-26 19:38:18 -0200

    removed old issues

--- fix-errors.sh
+++ fix-errors.sh
@@ -1,14 +1,6 @@
 # make sure that the final result is placed on
 # bosque_C{P,F}.udep.workaround.conll and does NOT update the original
 
-# fix issue #2
-cat bosque_CP.udep.conll | sed -e 's/Gender=None/Gender=Fem,Masc/' > tmp && mv tmp bosque_CP.udep.workaround.conll
-cat bosque_CF.udep.conll | sed -e 's/Gender=None/Gender=Fem,Masc/' > tmp && mv tmp bosque_CF.udep.workaround.conll
-
-# fix issue 24
-cat bosque_CP.udep.workaround.conll | sed -e 's/Number=None/Number=Plur,Sing/' > tmp && mv tmp bosque_CP.udep.workaround.conll
-cat bosque_CF.udep.workaround.conll | sed -e 's/Number=None/Number=Plur,Sing/' > tmp && mv tmp bosque_CF.udep.workaround.conll
-
 # fix issues #19 and #30
 sbcl --noinform --noprint --non-interactive --load join-features.lisp --eval '(in-package :join-features)' --eval '(execute "bosque_CP.udep.workaround.conll" "tmp1")' --eval '(execute "bosque_CF.udep.workaround.conll" "tmp2")'
 		
