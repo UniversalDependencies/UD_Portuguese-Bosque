fe99c7919a1343dc78b7e7067b2f17b98e514049
Fabricio Rosario <fchalub@br.ibm.com>
2016-10-27 10:03:01 -0200

    added missing comment to prepare-release

--- prepare-release.sh
+++ prepare-release.sh
@@ -20,4 +20,5 @@ cat pt.conllu | sed '/./,$!d' > tmp && mv tmp pt.conllu
 # remove empty line after the comment (flagged by the validation tool)
 python3 fix-comments.py pt.conllu > tmp && mv tmp pt.conllu
 
+# split pt.conllu in dev/test/train, using the percentages specified in the split3.py file
 python3 split3.py pt.conllu pt-ud-dev.conllu pt-ud-test-conllu pt-ud-train.conllu
