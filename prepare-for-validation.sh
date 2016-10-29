9af481bed586346b2475d7279dc9461dd3c1d9da
Fabricio Rosario <fchalub@br.ibm.com>
2016-10-29 20:16:10 -0200

    fixing release scripts; fixed validation script.  closes #67.

--- prepare-for-validation.sh
+++ prepare-for-validation.sh
@@ -2,5 +2,4 @@
 # of the UD tools repository and execute something like this:
 # python validate.py --max-err=0 --lang=pt2 <file>
 
-cat bosque_CP.udep.workaround.conll | grep -v "^#" > tmp && cat -s tmp > bosque_CP.udep.validation.conll
-cat bosque_CF.udep.workaround.conll | grep -v "^#" > tmp && cat -s tmp > bosque_CF.udep.validation.conll
+cat bosque.udep.workaround.conll | grep -v "^#" > tmp && cat -s tmp > bosque.udep.validation.conll
