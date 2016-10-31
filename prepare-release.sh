8bec29629bd3771e521b447af2a51c6023c6892f
Fabricio Rosario <fchalub@br.ibm.com>
2016-10-31 10:39:31 -0200

    renaming the files back.

--- prepare-release.sh
+++ prepare-release.sh
@@ -16,5 +16,5 @@ cat pt.conllu | sed '/./,$!d' > tmp && mv tmp pt.conllu
 python3 fix-comments.py pt.conllu > tmp && mv tmp pt.conllu
 
 # split pt.conllu in dev/test/train, using the percentages specified in the split3.py file
-python3 split3.py pt.conllu release/pt_bosque-ud-dev.conllu release/pt_bosque-ud-test.conllu release/pt_bosque-ud-train.conllu
+python3 split3.py pt.conllu release/pt-ud-dev.conllu release/pt-ud-test.conllu release/pt-ud-train.conllu
 rm pt.conllu
