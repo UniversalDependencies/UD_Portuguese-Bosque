8cc1944e0b749acebbb12b6bf4fdec882f64807d
Fabricio Rosario <fchalub@br.ibm.com>
2016-12-02 18:00:45 -0200

    removed obsolete script

--- scripts/prepare-for-validation.sh
+++
@@ -1,5 +0,0 @@
-# to validate, copy the files in validation/ to the data/ directory
-# of the UD tools repository and execute something like this:
-# python validate.py --max-err=0 --lang=pt2 <file>
-
-cat bosque.udep.workaround.conll | grep -v "^#" > tmp && cat -s tmp > bosque.udep.validation.conll
