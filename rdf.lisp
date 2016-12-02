410154332f8ac3f08ec28314bb593b5680492782
Fabricio Rosario <fchalub@br.ibm.com>
2016-12-02 14:26:50 -0200

    fixed metadata for sentence id

--- rdf.lisp
+++ rdf.lisp
@@ -6,4 +6,4 @@
   (with-open-file (out "bosque.ttl" :direction :output :if-exists :supersede)
     (convert-conll out (read-conllu "bosque.udep.conll")
                    (lambda (s) (cdr (assoc "text" (sentence-meta s) :test #'equal)))
-                   (lambda (s) (cdr (assoc "ref" (sentence-meta s) :test #'equal))))))
+                   (lambda (s) (cdr (assoc "sent_id" (sentence-meta s) :test #'equal))))))
