ba173f9c1b86dd04865397c128cda3bac49f69dc
Fabricio Rosario <fchalub@br.ibm.com>
2017-02-13 13:47:33 -0200

    fixing some bugs for fixing issue #160

--- scripts/fix-issue-160.lisp
+++ scripts/fix-issue-160.lisp
@@ -4,6 +4,33 @@
 
 ;; awk '$2 ~ /\-$/ && $4 ~ /VERB/ {vb = $0 ; getline ; if ($4 !~ /PRON/) print vb,"\n",$0,FILENAME }' ../documents/*.conllu
 
+(defun collect-next-element (list)
+  (loop for (x y)
+     on list collect (cons x y)))
+
+(defun has-feature (key features)
+  (unless (string-equal "_" features)
+    (find key (split-sequence #\| features) :key (lambda (x) (first (split-sequence #\= x))) :test #'equal)))
+
+(defun remove-feature* (key features)
+  (format nil "~{~a~^|~}" (remove-if (lambda (x) (equal key (car (split-sequence #\= x)))) (split-sequence #\| features))))
+
+(defun remove-feature (key value features)
+  (format nil "~{~a~^|~}" (remove-if (lambda (x) (equal `(,key ,value) (split-sequence #\= x))) (split-sequence #\| features))))
+
+(defun append-feature (key value feats)
+  (let* ((features (if (string= "_" feats) nil
+                       (split-sequence #\| feats))))
+    (format nil "~{~a~^|~}" (remove-duplicates
+                             (sort
+                              (append features `(,(if (and key value) 
+                                                      (format nil "~a=~a" key value)
+                                                      (format nil "~a" value)))) #'string<)
+                             :test #'equal))))
+
+(defun insert-mtoken (s mt)
+  (setf (sentence-mtokens s) (cons mt (sentence-mtokens s))))
+
 (defun fix-enclisis (sentence)
   (labels ((next-token (tk)
 	     (find (1+ (token-id tk))
@@ -12,33 +39,35 @@
     (let ((verb-enclisis (remove-if-not (lambda (tk)
 					  (and
 					   (scan "-$" (token-form tk)) 
-					   (equal (token-upostag tk) "VERB")
-					   (equal (token-upostag (next-token tk))
-						  "PRON")))
+					   (or (equal (token-upostag tk) "VERB")
+                                               (equal (token-upostag tk) "AUX"))
+					   (equal (token-upostag (next-token tk)) "PRON")))
 					(sentence-tokens sentence))))
       (dolist (tk verb-enclisis)
-	(insert-mtoken sentence
-		       (make-instance 'mtoken
-				      :form (concatenate
-					     'string
-					     (token-form tk)
-					     (token-form (next-token
-							  tk)))
-				      :start (token-id tk)
-				      :end (1+ (token-id tk))))
+        (let* ((nt (next-token tk))
+               (mt (make-instance 'mtoken
+                                  :form (concatenate 'string (token-form tk) (token-form nt))
+                                  :start (token-id tk)
+                                  :end (token-id nt))))
+          (when (has-feature "SpaceAfter" (token-misc tk))
+            (setf (token-misc tk) (remove-feature* "SpaceAfter" (token-misc tk)))
+            (setf (token-misc tk) (remove-feature "ChangedBy" "Issue165" (token-misc tk)))
+            (when (equal "" (token-misc tk))
+              (setf (token-misc tk) "_")))
+          (when (has-feature "SpaceAfter" (token-misc nt))
+            (setf (token-misc nt) (remove-feature* "SpaceAfter" (token-misc nt)))
+            (setf (token-misc nt) (remove-feature "ChangedBy" "Issue165" (token-misc nt)))
+            (when (equal "" (token-misc nt))
+              (setf (token-misc nt) "_"))
+            (setf (mtoken-misc mt) (append-feature "SpaceAfter" "No" (mtoken-misc mt))))
+          (format t "Inserting [~a] (~a-~a)~%" (mtoken-form mt) (mtoken-start mt) (mtoken-end mt))
+          (insert-mtoken sentence mt))
 	(setf (token-form tk)
 	      (regex-replace "-$" (token-form tk) "")))
       (if verb-enclisis
 	  (values sentence t)
 	  (values sentence nil)))))
 			 
-
 (defun run ()
   (dolist (fn (directory "documents/C*.conllu"))
-    (let* ((path (pathname-directory fn))
-	   (fn-out (make-pathname :type "new"
-				  :directory path
-				  :defaults fn)))
-      (write-conllu (mapcar #'fix-enclisis 
-			    (read-conllu fn))
-		    fn-out))))
+    (write-conllu (mapcar #'fix-enclisis (read-conllu fn)) fn)))
