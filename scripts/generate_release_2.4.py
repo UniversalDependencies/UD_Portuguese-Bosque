26e41ce0a7ba5508ef9d3f31787de2af2c8f006d
Elvis de Souza <elvis.desouza99@gmail.com>
2019-04-23 21:15:08 -0300

    Adição de scripts para gerar release 2.4

--- scripts/generate_release_2.4.py
+++ scripts/generate_release_2.4.py
@@ -31,6 +31,8 @@ for arquivo_id in os.listdir(pasta):
 				if (token.upos == "ADJ" or token.upos == "NOUN") and "VerbForm=Ger" in token.feats: token.feats = token.feats.replace("VerbForm=Ger", "").replace("||", "|")
 				if (token.upos == "ADJ" or token.upos == "NOUN") and "VerbForm=Inf" in token.feats: token.feats = token.feats.replace("VerbForm=Inf", "").replace("||", "|")
 				if (token.upos == "ADJ" or token.upos == "NOUN") and "VerbForm=Part" in token.feats: token.feats = token.feats.replace("VerbForm=Part", "").replace("||", "|")
+				if token.upos == "ADJ" and token.deprel == "acl": token.deprel = "amod"
+				if token.upos == "NOUN" and token.deprel == "acl": token.deprel = "nmod"
 				if token.feats == "": token.feats = "_"
 				if token.feats[0] == "|": token.feats = token.feats[1:]
 				if token.feats[-1] == "|": token.feats = token.feats[:-1]
@@ -39,7 +41,7 @@ for arquivo_id in os.listdir(pasta):
 		novo_conllu = list()
 		for i, identificador in enumerate(ids.splitlines()):
 			if identificador.strip() != '':
-				novo_conllu.append(corpus[identificador].to_str())
+				novo_conllu.append(corpus.sentences[identificador].to_str())
 				print(arquivo_conllu.rsplit('/', 1)[1] + ' - ' + str(i+1) + '/' + str(len(ids.splitlines())) + ': ' + identificador)
 
 		open(arquivo_conllu, 'w').write("\n\n".join(novo_conllu) + '\n\n')
