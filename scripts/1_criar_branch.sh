80cb907c7ad5a3f7ad625d055d1a76389f07586a
Elvis de Souza <elvis.desouza99@gmail.com>
2020-04-17 02:11:36 -0300

    fix scripts

--- scripts/1_criar_branch.sh
+++ scripts/1_criar_branch.sh
@@ -18,11 +18,5 @@ git checkout -b $1 workbench
 git push --set-upstream origin $1
 cat documents/*.conllu > Inalterados/$1.conllu
 sudo chmod a+rwx Inalterados/$1.conllu
-if [ $USER != "elvis_desouza99" ]; then
-	rsync -av -e 'ssh -i /home/elvis/.ssh/google_compute_engine' Inalterados/$1.conllu elvis_desouza99@35.196.100.118:~/Dropbox/tronco/comcorhd.tronco.me/$pasta/www/interrogar-ud/conllu/
-	cp Inalterados/$1.conllu ~/Dropbox/tronco/comcorhd.tronco.me/$pasta/www/interrogar-ud/conllu/
-else
-	cp Inalterados/$1.conllu ~/Dropbox/tronco/comcorhd.tronco.me/$pasta/www/interrogar-ud/conllu/
-	sudo chmod a+rwx ~/Dropbox/tronco/comcorhd.tronco.me/$pasta/www/interrogar-ud/conllu/$1.conllu
-fi
+cp Inalterados/$1.conllu ../Interrogat-rio/www/interrogar-ud/conllu/
 git checkout workbench
